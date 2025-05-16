11dcb09aab7d63eeca115052f985129162411c5c
= <=>
2025-05-16 14:33:19 +0300

    Initial commit - Updated portfolio

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Portfolio Website Loaded!");
+    const navToggle = document.getElementById("nav-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("nav-menu-visible");
+        navToggle.classList.toggle("bx-x"); // changes menu icon to 'x'
+    });
 });
